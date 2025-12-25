5b2480611b77f4ad32da02376601a14dc53cd19c
Ahmed Farid <ahmedferid0@gmail.com>
2025-12-25 14:44:08 +0100

    updating enemuration phase

---
+++ charts/SSH_chart.mm
@@ -0,0 +1,213 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks>
+    <bookmark nodeId="ID_1090958577" name="Root" opensAsRoot="true"/>
+</bookmarks>
+<node TEXT="SSH" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1766654326938">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#009999ff" zoom="0.4665073">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_grey_template.mm" show_tags="UNDER_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_364653791" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#676767" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_364653791" STARTINCLINATION="122.5 pt;0 pt;" ENDINCLINATION="119 pt;35.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#444444" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#3333ff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#3333ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1533685614" COLOR="#443333" BACKGROUND_COLOR="#dddddd" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc0033" TRANSPARENCY="255" DESTINATION="ID_1533685614"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#cc0033"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#000000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#222222" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#444444" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#676767" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#888888" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#222222" BACKGROUND_COLOR="#aaaaaa" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#222222" BACKGROUND_COLOR="#bbbbbb" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#222222" BACKGROUND_COLOR="#cccccc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#222222" BACKGROUND_COLOR="#dddddd" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#222222" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="SSH (Secure Shell) is a cryptographic network protocol for secure remote login and command execution, data transfer, and other network services, encrypting all communication.                                                                                              Click the icon above to further redirect to RFC4253 write-up." POSITION="bottom_or_right" ID="ID_1028570036" CREATED="1766654356683" MODIFIED="1766667559174" LINK="https://datatracker.ietf.org/doc/html/rfc4253#section-1">
+<node TEXT="SSH Handshake" ID="ID_1461055263" CREATED="1766654603325" MODIFIED="1766654648263">
+<node TEXT="The SSH handshake is the process where the client and server agree on security settings and create encryption keys so they can communicate safely." ID="ID_1461487870" CREATED="1766654652330" MODIFIED="1766654716503">
+<node TEXT="Participants" ID="ID_143814809" CREATED="1766654762540" MODIFIED="1766654796104">
+<node TEXT="" ID="ID_1048992027" CREATED="1766667017889" MODIFIED="1766667017889">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="SSH Client" ID="ID_329697934" CREATED="1766654856850" MODIFIED="1766654977682">
+<node TEXT="Initiates connection" ID="ID_1771145915" CREATED="1766655612290" MODIFIED="1766666132474"/>
+<node TEXT="Sends supported algorithms" ID="ID_322529698" CREATED="1766666109102" MODIFIED="1766666155172"/>
+<node TEXT="Verifires server identity" ID="ID_700475357" CREATED="1766666106269" MODIFIED="1766666173689"/>
+</node>
+<node TEXT="SSH Server" ID="ID_1698732428" CREATED="1766654863177" MODIFIED="1766654986776">
+<node TEXT="Accepts connection" ID="ID_1995360031" CREATED="1766655615499" MODIFIED="1766666190100"/>
+<node TEXT="Sends supported algorithms" ID="ID_941566722" CREATED="1766666114955" MODIFIED="1766666206299"/>
+<node TEXT="Sends host key for verification" ID="ID_142477424" CREATED="1766666119239" MODIFIED="1766666225887"/>
+</node>
+<node TEXT="" ID="ID_1566366034" CREATED="1766667017887" MODIFIED="1766667017889">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="The client starts the connection and proposes algorithms, while the server responds with its own preferences and host key. Both sides then verify and agree on how the session will be secured." ID="ID_183055889" CREATED="1766667017889" MODIFIED="1766667021114"/>
+</node>
+</node>
+<node TEXT="Handshake Steps" ID="ID_130084760" CREATED="1766654771516" MODIFIED="1766654836582">
+<node TEXT="Version Echange" ID="ID_25352628" CREATED="1766654868867" MODIFIED="1766655009127">
+<node TEXT="" ID="ID_989258533" CREATED="1766667762385" MODIFIED="1766667762385">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Client &gt; server" ID="ID_1223386896" CREATED="1766666237016" MODIFIED="1766666265017">
+<node TEXT="Send SSH protocol version" ID="ID_1396268009" CREATED="1766666265035" MODIFIED="1766666274374"/>
+</node>
+<node TEXT="Server &gt; client" ID="ID_1876188329" CREATED="1766666241805" MODIFIED="1766666284225">
+<node TEXT="Sends SSH protocol version" ID="ID_1875011504" CREATED="1766666284235" MODIFIED="1766666291546"/>
+</node>
+<node TEXT="" ID="ID_975938031" CREATED="1766667762384" MODIFIED="1766667762385">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Both sides exchange their protocol versions (e.g., SSH-2.0) to confirm they support the same SSH version. This step ensures compatibility before any encryption or authentication." ID="ID_1803016206" CREATED="1766667762386" MODIFIED="1766667771248"/>
+</node>
+</node>
+<node TEXT="Key Exchange Initialization" ID="ID_1847094969" CREATED="1766654921819" MODIFIED="1766655016480">
+<node TEXT="" ID="ID_1987551154" CREATED="1766667786077" MODIFIED="1766667786077">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Client &gt; Server" ID="ID_1198769863" CREATED="1766666304550" MODIFIED="1766666316395">
+<node TEXT="SSH_MSG_KEYINIT, which lists includes the supported algorithms from the client side" ID="ID_544695806" CREATED="1766666316411" MODIFIED="1766666343265"/>
+</node>
+<node TEXT="Server &gt; Client" ID="ID_1773758760" CREATED="1766666308287" MODIFIED="1766666351024">
+<node TEXT="SSH_MSG_KEXINIT, which lists includes the supported algorithms from the server side" ID="ID_1543240416" CREATED="1766666351034" MODIFIED="1766666380474"/>
+</node>
+<node TEXT="" ID="ID_111411182" CREATED="1766667786077" MODIFIED="1766667786077">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Each side sends a KEXINIT message containing supported key exchange, encryption, MAC, and compression algorithms. This allows both sides to see which algorithms are compatible." ID="ID_372077343" CREATED="1766667786077" MODIFIED="1766667788474"/>
+</node>
+</node>
+<node TEXT="Algorithm Agreement" ID="ID_21099622" CREATED="1766654928006" MODIFIED="1766655029652">
+<node TEXT="" ID="ID_1561354876" CREATED="1766667814593" MODIFIED="1766667814593">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Negotiated key exchange algorithm (KEX)" ID="ID_1444052426" CREATED="1766666781500" MODIFIED="1766666807404"/>
+<node TEXT="Negotiated encryption algorithm" ID="ID_148637286" CREATED="1766666793992" MODIFIED="1766666811359"/>
+<node TEXT="Negotiated MAC algorithm" ID="ID_1764308600" CREATED="1766666790737" MODIFIED="1766666815647"/>
+<node TEXT="Negotiated compression algorithm" ID="ID_1571912928" CREATED="1766666786640" MODIFIED="1766666820075"/>
+<node TEXT="" ID="ID_794472029" CREATED="1766667814586" MODIFIED="1766667814593">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Both sides pick the first algorithm in common from their lists. This negotiation ensures that the client and server use the same algorithms for encryption, integrity, and optional compression." ID="ID_1585048274" CREATED="1766667814594" MODIFIED="1766667816954">
+<node TEXT="For example: Client sends cipher of what it receives &amp; what it sends ( aes128, aes256 ) + (aes256, aes128), and the server sends both side as well, Which concludes by the client working with the first match ( same procedure from the server side as well )." ID="ID_100218229" CREATED="1766667818077" MODIFIED="1766667931894"/>
+</node>
+</node>
+</node>
+<node TEXT="Key Exchange" ID="ID_1423257638" CREATED="1766654941005" MODIFIED="1766655035284">
+<node TEXT="" ID="ID_709614643" CREATED="1766667950454" MODIFIED="1766667950454">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Client and server perform KEX" ID="ID_832879395" CREATED="1766666832156" MODIFIED="1766666841494"/>
+<node TEXT="Generate shared secret agreed on" ID="ID_1603157684" CREATED="1766666833511" MODIFIED="1766666863784"/>
+<node TEXT="" ID="ID_135234611" CREATED="1766667950454" MODIFIED="1766667950454">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="The agreed key exchange algorithm is used to securely create a shared secret over an insecure network, without sending the secret directly" ID="ID_1721181599" CREATED="1766667950454" MODIFIED="1766667952754"/>
+</node>
+</node>
+<node TEXT="Shared Secret &amp; Key Derivation" ID="ID_802747504" CREATED="1766654947865" MODIFIED="1766655036462">
+<node TEXT="" ID="ID_121769204" CREATED="1766668003905" MODIFIED="1766668003905">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Derive session keys from shared secret" ID="ID_73921302" CREATED="1766666867354" MODIFIED="1766666886312"/>
+<node TEXT="Prepare encryption and MAC keys" ID="ID_600653098" CREATED="1766666879099" MODIFIED="1766666891734"/>
+<node TEXT="" ID="ID_1007432012" CREATED="1766668003904" MODIFIED="1766668003905">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Using the shared secret and exchanged parameters, both sides generate symmetric keys for encryption and MAC, ensuring that future communication is confidential and authenticated." ID="ID_581062342" CREATED="1766668003907" MODIFIED="1766668006744"/>
+</node>
+</node>
+<node TEXT="New Keys Activation" ID="ID_1322663288" CREATED="1766654951107" MODIFIED="1766655049516">
+<node TEXT="" ID="ID_917494304" CREATED="1766668020105" MODIFIED="1766668020105">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Client switches to encrypted channel" ID="ID_697846048" CREATED="1766666893525" MODIFIED="1766666908594"/>
+<node TEXT="Server switches to encrypted channel" ID="ID_1707230834" CREATED="1766666911438" MODIFIED="1766666920858"/>
+<node TEXT="" ID="ID_1261804457" CREATED="1766668020104" MODIFIED="1766668020105">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="After keys are derived, both sides activate encryption and integrity protection. All subsequent traffic is encrypted using the negotiated algorithms and keys." ID="ID_284299185" CREATED="1766668020105" MODIFIED="1766668021704"/>
+</node>
+</node>
+<node TEXT="Secure Transport Established" ID="ID_1150207993" CREATED="1766654954628" MODIFIED="1766655054041">
+<node TEXT="" ID="ID_1414911450" CREATED="1766668029504" MODIFIED="1766668029504">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Encrypted communication begins" ID="ID_1759821427" CREATED="1766666924175" MODIFIED="1766666940559"/>
+<node TEXT="Authentication (password, public key, etc.) occurs" ID="ID_695875810" CREATED="1766666926161" MODIFIED="1766666941864"/>
+<node TEXT="" ID="ID_19064380" CREATED="1766668029504" MODIFIED="1766668029504">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="The secure channel is now active. Authentication follows, allowing the client to log in safely. From this point, all data is encrypted and integrity-protected." ID="ID_1850824103" CREATED="1766668029514" MODIFIED="1766668031199"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
